aaa99257c7226ca16d2bb8f7fb8e1018a2558389
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-03-01 09:36:13 -0600

    fresh start

--- vectorclass/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qlu/s2-devel/build/synergia2/src/synergia/utils/vectorclass
-# Build directory: /home/qlu/s2-devel/build/synergia2/src/synergia/utils/vectorclass
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
