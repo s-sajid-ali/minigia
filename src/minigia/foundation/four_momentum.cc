442e6ee1867095089e9b007801087c86d442f2e4
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-03-09 17:09:47 -0600

    minor

--- src/minigia/foundation/four_momentum.cc
+++ src/minigia/foundation/four_momentum.cc
@@ -128,25 +128,3 @@ Four_momentum::serialize(Archive & ar, const unsigned int version)
         & CEREAL_NVP(beta);
 }
 #endif
-
-#if 0
-template
-void
-Four_momentum::serialize<boost::archive::binary_oarchive >(
-        boost::archive::binary_oarchive & ar, const unsigned int version);
-
-template
-void
-Four_momentum::serialize<boost::archive::xml_oarchive >(
-        boost::archive::xml_oarchive & ar, const unsigned int version);
-
-template
-void
-Four_momentum::serialize<boost::archive::binary_iarchive >(
-        boost::archive::binary_iarchive & ar, const unsigned int version);
-
-template
-void
-Four_momentum::serialize<boost::archive::xml_iarchive >(
-        boost::archive::xml_iarchive & ar, const unsigned int version);
-#endif
