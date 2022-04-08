b1adbb363743eee7d6f7e64db685ff6c518ed8a8
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-04-08 10:31:44 -0500

    minor

--- misc/diagnostics_mean_profile.cc
+++ misc/diagnostics_mean_profile.cc
@@ -13,98 +13,96 @@ int main(int argc, char *argv[]) {
   Kokkos::initialize(argc, argv);
   Commxx comm;
 
+  const double mass = 100.0;
+  const double total_energy = 125.0;
+  double real_particles = 10485760;
+  double macroparticles = 2940000000000.0;
+
+  Four_momentum four_momentum(pconstants::mp);
+  Reference_particle refpart(1, four_momentum);
+
+  Logger screen(0, LoggerV::INFO);
+  Bunch bunch;
+  bunch.read_file(std::string("particles.h5"));
+
   {
-    const double mass = 100.0;
-    const double total_energy = 125.0;
-    double real_particles = 10485760;
-    double macroparticles = 2940000000000.0;
-
-    Four_momentum four_momentum(pconstants::mp);
-    Reference_particle refpart(1, four_momentum);
-
-    Logger screen(0, LoggerV::INFO);
-    Bunch bunch;
-    bunch.read_file(std::string("particles.h5"));
-
-    {
-      scoped_simple_timer timer("old_method");
-      for (int run = 0; run < 100; run++) {
-        auto mean = Core_diagnostics::calculate_mean(bunch);
-        auto stddev = Core_diagnostics::calculate_std(bunch, mean);
-      }
+    scoped_simple_timer timer("old_method");
+    for (int run = 0; run < 100; run++) {
+      auto mean = Core_diagnostics::calculate_mean(bunch);
+      auto stddev = Core_diagnostics::calculate_std(bunch, mean);
     }
-
-    const auto particles = bunch.get_local_particles();
-    const auto masks = bunch.get_local_particle_masks();
-
-    const auto total_bunch_particles = bunch.get_total_num();
-    const auto local_bunch_capacity = bunch.size();
-
-    Kokkos::View<double[6], Kokkos::DefaultHostExecutionSpace> mean_and_stddev(
-        "mean_stddev");
-    // std::array<double, 6> mean_and_stddev{0, 0, 0, 0, 0, 0};
-    {
-      scoped_simple_timer timer("new_method");
-
-      auto instances = Kokkos::Experimental::partition_space(
-          Kokkos::DefaultExecutionSpace(), 1, 1, 1);
-      for (int run = 0; run < 100; run++) {
-
-        for (int instance_id = 0; instance_id < 3; instance_id++) {
-          // std::cout << "starting 1st set of instances with id : " <<
-          // instance_id
-          //                   << " at " << MPI_Wtime() << "\n";
-          int idx1 = 0 + instance_id;
-          int idx2 = 3 + instance_id;
-          int idx3 = 2 * instance_id;
-          Kokkos::parallel_reduce(
-              "position_sum",
-              Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(
-                  instances[instance_id], 0, local_bunch_capacity),
-              KOKKOS_LAMBDA(const int &i, double &sum_pos) {
-                if (masks(i)) {
-                  sum_pos += particles(i, idx3);
-                }
-              },
-              mean_and_stddev[idx1]);
-          Kokkos::parallel_reduce(
-              "position_sumsquares",
-              Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(
-                  instances[instance_id], 0, local_bunch_capacity),
-              KOKKOS_LAMBDA(const int &i, double &sum_squarepos) {
-                if (masks(i)) {
-                  sum_squarepos += particles(i, idx3) * particles(i, idx3);
-                }
-              },
-              mean_and_stddev[idx2]);
-        }
-        for (int instance_id = 0; instance_id < 3; instance_id++) {
-          // std::cout << "starting 2nd set of instances with id : " <<
-          // instance_id
-          //           << " at " << MPI_Wtime() << "\n";
-          int idx1 = 0 + instance_id;
-          int idx2 = 3 + instance_id;
-          instances[instance_id].fence();
-        }
-
-        int status;
-        status = MPI_Allreduce(MPI_IN_PLACE, mean_and_stddev.data(), 6,
-                               MPI_DOUBLE, MPI_SUM, bunch.get_comm());
-        if (status != MPI_SUCCESS) {
-          std::cout << status << "\n";
-        }
-
-        /* mean_and_stddev has sum_and_sumsquares on each MPI rank */
-        for (int j = 0; j < 3; j++) {
-          mean_and_stddev[j] = mean_and_stddev[j] / total_bunch_particles;
-          mean_and_stddev[j + 3] =
-              std::sqrt((mean_and_stddev[j + 3]) / total_bunch_particles -
-                        std::pow(mean_and_stddev[j], 2));
-        }
-      }
-    }
-    simple_timer_print(screen);
   }
+
+  const auto particles = bunch.get_local_particles();
+  const auto masks = bunch.get_local_particle_masks();
+
+  const auto total_bunch_particles = bunch.get_total_num();
+  const auto local_bunch_capacity = bunch.size();
+
+  Kokkos::View<double[6], Kokkos::DefaultHostExecutionSpace> mean_and_stddev(
+      "mean_stddev");
+  // std::array<double, 6> mean_and_stddev{0, 0, 0, 0, 0, 0};
+  {
+    scoped_simple_timer timer("new_method");
+
+    auto instances = Kokkos::Experimental::partition_space(
+        Kokkos::DefaultExecutionSpace(), 1, 1, 1);
+    for (int run = 0; run < 100; run++) {
+
+      for (int instance_id = 0; instance_id < 3; instance_id++) {
+        // std::cout << "starting 1st set of instances with id : " <<
+        // instance_id
+        //                   << " at " << MPI_Wtime() << "\n";
+        int idx1 = 0 + instance_id;
+        int idx2 = 3 + instance_id;
+        int idx3 = 2 * instance_id;
+        Kokkos::parallel_reduce(
+            "position_sum",
+            Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(
+                instances[instance_id], 0, local_bunch_capacity),
+            KOKKOS_LAMBDA(const int &i, double &sum_pos) {
+              if (masks(i)) {
+                sum_pos += particles(i, idx3);
+              }
+            },
+            mean_and_stddev[idx1]);
+        Kokkos::parallel_reduce(
+            "position_sumsquares",
+            Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(
+                instances[instance_id], 0, local_bunch_capacity),
+            KOKKOS_LAMBDA(const int &i, double &sum_squarepos) {
+              if (masks(i)) {
+                sum_squarepos += particles(i, idx3) * particles(i, idx3);
+              }
+            },
+            mean_and_stddev[idx2]);
+      }
+      for (int instance_id = 0; instance_id < 3; instance_id++) {
+        // std::cout << "starting 2nd set of instances with id : " <<
+        // instance_id
+        //           << " at " << MPI_Wtime() << "\n";
+        int idx1 = 0 + instance_id;
+        int idx2 = 3 + instance_id;
+        instances[instance_id].fence();
+      }
+
+      int status;
+      status = MPI_Allreduce(MPI_IN_PLACE, mean_and_stddev.data(), 6,
+                             MPI_DOUBLE, MPI_SUM, bunch.get_comm());
+      if (status != MPI_SUCCESS) {
+        std::cout << status << "\n";
+      }
+
+      /* mean_and_stddev has sum_and_sumsquares on each MPI rank */
+      for (int j = 0; j < 3; j++) {
+        mean_and_stddev[j] = mean_and_stddev[j] / total_bunch_particles;
+        mean_and_stddev[j + 3] =
+            std::sqrt((mean_and_stddev[j + 3]) / total_bunch_particles -
+                      std::pow(mean_and_stddev[j], 2));
+      }
+    }
+  }
+  simple_timer_print(screen);
 
   Kokkos::finalize();
   MPI_Finalize();
