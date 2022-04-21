c159ab36c40ccb1c27e47e8c5e9697193c007aee
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-04-21 09:36:34 -0500

    minor

--- src/minigia/collective/deposit.hpp
+++ src/minigia/collective/deposit.hpp
@@ -3,6 +3,8 @@
 
 #include <minigia/bunch/bunch.hpp>
 
+#include "rectangular_grid_domain.hpp"
+
 void deposit_charge_rectangular_3d_kokkos_scatter_view(
     karray1d_dev &rho_dev, Rectangular_grid_domain &domain,
     std::array<int, 3> const &dims, Bunch const &bunch);
