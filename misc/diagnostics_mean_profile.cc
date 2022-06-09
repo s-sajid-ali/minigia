1f214509c7fb1846cab6953b75de3a8b637cc9c2
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-06-09 15:02:55 -0500

    core-diagnositcs new impl minor refactor

--- misc/diagnostics_mean_profile.cc
+++ misc/diagnostics_mean_profile.cc
@@ -25,7 +25,6 @@ int main(int argc, char *argv[]) {
     scoped_simple_timer timer("100its of old_method");
     for (int run = 0; run < 100; run++) {
       auto mean = Core_diagnostics::calculate_mean(bunch);
-      auto stddev = Core_diagnostics::calculate_std(bunch, mean);
     }
   }
 
@@ -35,57 +34,56 @@ int main(int argc, char *argv[]) {
   const auto total_bunch_particles = bunch.get_total_num();
   const auto local_bunch_capacity = bunch.size();
 
-  Kokkos::View<double[6], Kokkos::DefaultHostExecutionSpace> mean_and_stddev(
-      "mean_stddev");
+  Kokkos::View<double[6], Kokkos::DefaultHostExecutionSpace> mean("cal_mean");
+
   {
     scoped_simple_timer timer("100its of new_method");
 
     auto instances = Kokkos::Experimental::partition_space(
         Kokkos::DefaultExecutionSpace(), 1, 1, 1);
+
+    /* spatial mean and stddev! */
     for (int run = 0; run < 100; run++) {
 
       for (int instance_id = 0; instance_id < 3; instance_id++) {
         int dst_idx1 = 0 + instance_id;
         int dst_idx2 = 3 + instance_id;
-        int src_idx = 2 * instance_id;
+        int src_idx1 = 0 + instance_id;
+        int src_idx2 = 3 + instance_id;
         Kokkos::parallel_reduce(
             "position_sum",
             Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(
                 instances[instance_id], 0, local_bunch_capacity),
             KOKKOS_LAMBDA(const int &i, double &sum_pos) {
               if (masks(i)) {
-                sum_pos += particles(i, src_idx);
+                sum_pos += particles(i, src_idx1);
               }
             },
-            mean_and_stddev[dst_idx1]);
+            mean[dst_idx1]);
         Kokkos::parallel_reduce(
-            "position_sumsquares",
+            "momenta_sum",
             Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(
                 instances[instance_id], 0, local_bunch_capacity),
-            KOKKOS_LAMBDA(const int &i, double &sum_squarepos) {
+            KOKKOS_LAMBDA(const int &i, double &sum_momenta) {
               if (masks(i)) {
-                sum_squarepos += particles(i, src_idx) * particles(i, src_idx);
+                sum_momenta += particles(i, src_idx2);
               }
             },
-            mean_and_stddev[dst_idx2]);
+            mean[dst_idx2]);
       }
       for (int instance_id = 0; instance_id < 3; instance_id++) {
         instances[instance_id].fence();
       }
 
       int status;
-      status = MPI_Allreduce(MPI_IN_PLACE, mean_and_stddev.data(), 6,
-                             MPI_DOUBLE, MPI_SUM, bunch.get_comm());
+      status = MPI_Allreduce(MPI_IN_PLACE, mean.data(), 6, MPI_DOUBLE, MPI_SUM,
+                             bunch.get_comm());
       if (status != MPI_SUCCESS) {
         std::cout << status << "\n";
       }
 
-      /* mean_and_stddev has sum_and_sumsquares on each MPI rank */
       for (int j = 0; j < 3; j++) {
-        mean_and_stddev[j] = mean_and_stddev[j] / total_bunch_particles;
-        mean_and_stddev[j + 3] =
-            std::sqrt((mean_and_stddev[j + 3]) / total_bunch_particles -
-                      std::pow(mean_and_stddev[j], 2));
+        mean[j] = mean[j] / total_bunch_particles;
       }
     }
   }
