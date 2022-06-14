2aa84f482b26193199d8a8b9813575818e03d3e9
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-06-14 10:32:30 -0500

    restructure

---
+++ profiling/deposit/deposit_profile.cc
@@ -0,0 +1,25 @@
+#include <minigia/collective/deposit.hpp>
+#include <minigia/utils/logger.hpp>
+#include <minigia/utils/simple_timer.hpp>
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  Commxx comm;
+  Kokkos::ScopeGuard kokkos(argc, argv);
+
+  const double mass = 100.0;
+  const double total_energy = 125.0;
+  double real_particles = 10485760;
+  double macroparticles = 2940000000000.0;
+
+  Four_momentum four_momentum(pconstants::mp);
+  Reference_particle refpart(1, four_momentum);
+
+  Logger screen(0, LoggerV::INFO);
+  Bunch bunch;
+  bunch.read_file(std::string("particles.h5"));
+
+  MPI_Finalize();
+
+  return 0;
+}
