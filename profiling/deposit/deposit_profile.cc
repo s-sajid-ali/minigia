c3c7244ba8ac3e19621a9637eee06e9c4e2ec11c
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-06-14 19:03:49 -0500

    updates

--- profiling/deposit/deposit_profile.cc
+++ profiling/deposit/deposit_profile.cc
@@ -19,6 +19,23 @@ int main(int argc, char *argv[]) {
   Bunch bunch;
   bunch.read_file(std::string("particles.h5"));
 
+  Rectangular_grid_domain domain(
+      {32, 32, 128},
+      {0.045792656585374118, 0.021010777363394857, 0.10253620986616825},
+      {1.0473336722046451e-13, 5.7671527053847625e-14, 1.2976955845519457e-7},
+      true);
+
+  const std::array<int, 3> dims{66, 64, 256};
+  karray1d_dev rho_dev("rho2_dev", 1081344);
+
+  {
+    scoped_simple_timer timer("100its of old_method");
+    for (int run = 0; run < 100; run++) {
+      deposit_charge_rectangular_3d_kokkos_scatter_view(rho_dev, domain, dims,
+                                                        bunch);
+    }
+  }
+
   MPI_Finalize();
 
   return 0;
