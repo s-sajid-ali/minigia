4f912533099e7a278f9e8d57f393dcca4d512fa5
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-06-30 13:33:01 -0500

    updates

--- profiling/deposit/deposit_profile.cc
+++ profiling/deposit/deposit_profile.cc
@@ -1,6 +1,23 @@
+#include <minigia/bunch/core_diagnostics.hpp>
 #include <minigia/collective/deposit.hpp>
 #include <minigia/utils/logger.hpp>
 #include <minigia/utils/simple_timer.hpp>
+
+double get_smallest_non_tiny(double val, double other1, double other2,
+                             double tiny) {
+  double retval;
+  if (val > tiny) {
+    retval = val;
+  } else {
+    if ((other1 > tiny) && (other2 > tiny)) {
+      retval = std::min(other1, other2);
+    } else {
+      retval = std::max(other1, other2);
+    }
+  }
+
+  return retval;
+}
 
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
@@ -18,14 +35,34 @@ int main(int argc, char *argv[]) {
   Logger screen(0, LoggerV::INFO);
   Bunch bunch;
   bunch.read_file(std::string("particles.h5"));
-
-  Rectangular_grid_domain domain(
-      {32, 32, 128},
-      {0.045792656585374118, 0.021010777363394857, 0.10253620986616825},
-      {1.0473336722046451e-13, 5.7671527053847625e-14, 1.2976955845519457e-7},
-      true);
-
   const std::array<int, 3> dims{32, 32, 128};
+
+  auto mean = Core_diagnostics::calculate_mean(bunch);
+  auto std = Core_diagnostics::calculate_std(bunch, mean);
+
+  const double tiny = 1.0e-10;
+
+  const auto ix = Bunch::x;
+  const auto iy = Bunch::y;
+  const auto iz = Bunch::z;
+  if ((std[ix] < tiny) && (std[iy] < tiny) && (std[iz] < tiny)) {
+    throw std::runtime_error(
+        "Space_charge_3d_open_hockney_eigen::update_domain: "
+        "all three spatial dimensions have neglible extent");
+  }
+
+  std::array<double, 3> offset{mean[ix], mean[iy], mean[iz]};
+
+  int n_sigma = 8;
+
+  std::array<double, 3> size{
+      n_sigma * get_smallest_non_tiny(std[0], std[2], std[4], tiny),
+      n_sigma * get_smallest_non_tiny(std[2], std[0], std[4], tiny),
+      n_sigma * get_smallest_non_tiny(std[4], std[0], std[2], tiny)};
+
+  Rectangular_grid_domain domain =
+      Rectangular_grid_domain(dims, size, offset, false);
+
   karray1d_dev rho_dev("rho2_dev", dims[0] * dims[1] * dims[2]);
 
   {
